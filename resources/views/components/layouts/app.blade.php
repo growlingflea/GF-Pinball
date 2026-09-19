ae511e3689e4f19f919a444197f5756d8d5f35fc
Daniel Pflieger <daniel@growlingflea.com>
2026-09-18 19:08:21 -0700

    Added the meta tag

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,15 +8,15 @@
 
 
     <meta property="og:title" content="GF-Pinball">
-    <meta property="og:description" content="Good Fuckin Pinball">
-    <meta property="og:image" content="{{ asset('images/og-preview.png') }}">
+    <meta property="og:description" content="Play Some Fuckin Pinball">
+    <meta property="og:image" content="{{ asset('images/topnav.jpg') }}">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="website">
 
     {{-- iMessage/Android also fall back to these if og:title/description are missing, but including them explicitly doesn't hurt --}}
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="GF-Pinball">
-    <meta name="twitter:description" content="On-site pinball reference: cheat sheets, rules, and machine info while you're standing at the machine.">
+    <meta name="twitter:description" content="Play some fuckin pins.">
     <meta name="twitter:image" content="{{ asset('images/og-preview.png') }}">
 
 
